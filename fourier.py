039faa17449d9d1e32e4f1c77a450de85466a49f
Owen <butlero2001@outlook.com>
2021-10-19 22:33:31 -0400

    Combined fouriera() and fourierb() into fourier(). Added graphing functionality.

--- fourier.py
+++ fourier.py
@@ -1,6 +1,7 @@
 """ Import libraries """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 """ Define functions """
 # Use trapezoid rule to integrate a function numerically from a to b
@@ -16,38 +17,53 @@ def integrate(X, Y):
 
     return result * dx / 2
 
-def fouriera(X, Y, N):
-    a = []
-    deltax = X[-1] - X[0]
-    for n in range(N):
-        a.append((2/deltax)*integrate(X, Y*np.cos(2*np.pi*n*X/deltax)))
-    return a
-
-def fourierb(X, Y, N):
-    b = []
-    deltax = X[-1] - X[0]
-    for n in range(1, N):
-        b.append((2/deltax)*integrate(X, Y*np.sin(2*np.pi*n*X/deltax)))
-    return b
-
 def fourier(X, Y, N):
     a = []
     b = []
     deltax = X[-1] - X[0]
 
     for n in range(N):
-        a.append('')
-        b.append('')
+        a.append((2/deltax)*integrate(X, Y*np.cos(2*np.pi*n*X/deltax)))
+        b.append((2/deltax)*integrate(X, Y*np.sin(2*np.pi*n*X/deltax)))
     return a, b
+
+def f(x, a, b):
+    P = x[-1] - x[0]
+    func = np.zeros_like(x)
+    func += a[0]/2
+    for i in range(1, len(a)):
+        func += a[i] * np.cos(2*np.pi*i*x/P) + b[i] * np.sin(2*np.pi*i*x/P)
+    return func
 
 """ Main body """
 wavedata = np.loadtxt("wavedata.csv", delimiter = ",")
 X, Y = wavedata[:, 0], wavedata[:, 1]
 
-print(integrate(X, Y))
-print(fouriera(X, Y, 6))
-print(fourierb(X, Y, 6))
+iterations = 6
+a, b = fourier(X, Y, iterations)
+a10, b10 = fourier(X, Y, 10)
+
+fouriery = f(X, a, b)
+fouriery10 = f(X, a10, b10)
 
 """ Plotting and formatting """
-fig = plt.figure(figsize = (12, 9))
+fig = plt.figure(figsize = (15, 9))
+gs = gridspec.GridSpec(ncols = 2, nrows = 2, figure = fig)
+ax1 = fig.add_subplot(gs[0, 0], ylim = (min(a10 + b10) - 1, max(a10 + b10) + 1), xlabel = 'n', ylabel = 'Magnitude of corresponding cos term')
+ax2 = fig.add_subplot(gs[0, 1], ylim = (min(a10 + b10) - 1, max(a10 + b10) + 1), xlabel = 'n', ylabel = 'Magnitude of corresponding sin term')
+ax3 = fig.add_subplot(gs[1, 0])
+ax4 = fig.add_subplot(gs[1, 1], title = 'Hi')
+
+range = np.arange(0, 10, 1)
+ax1.bar(range, a10)
+ax2.bar(range, b10)
+
+ax3.plot(X, Y, label = 'Plot of input data')
+ax3.plot(X, fouriery, label = 'Plot of Fourier approximation with sin and cos terms up to n=6')
+ax3.legend()
+
+ax4.plot(X, Y, label = 'Plot of input data')
+ax4.plot(X, fouriery10, label = 'Plot of Fourier approximation with sin and cos terms up to n=10')
+ax4.legend()
+
 plt.show()
